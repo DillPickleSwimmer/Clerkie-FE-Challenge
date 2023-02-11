c5e2613da29d5631ca052c7d863bc14f0b74424b
Alexandra J <dillpickleswimmer@GMAIL.COM>
2023-02-11 14:23:36 -0800

    main layout

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import styles from './layout.module.css';
+
+export default function Layout({children}) {
+    return (
+        <>
+            <Head>
+                <title>Clerkie Frontend Challenge</title>
+                <meta name="description" content="A simple friends filtering application" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={styles.layout}>
+                <Navbar />
+                <main>
+                    {children}
+                </main>
+            </div>
+        </>
+    )
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styles from './navbar.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import {useRouter} from 'next/router';
+
+
+
+export default function Navbar() {
+    const router = useRouter()
+    const selectedPath = router.pathname;
+
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.title}>
+                <Image src="/icons/logo.svg" height={20} width={20} alt="Clerkie Logo" />
+                <span className={styles.titleCaption}>Clerkie Challenge</span>
+            </div>
+            <nav className={styles.nav}>
+                <NavbarLink path='/' icon='/icons/home.svg' caption='Home' selected={selectedPath === '/'} />
+                <NavbarLink path='/friends' icon='/icons/friends.svg' caption='Friends' selected={selectedPath?.startsWith('/friends')}/>
+            </nav>
+        </div>
+    )
+}
+
+function NavbarLink({path, icon, caption, selected = false}: {path: string, icon: string, caption: string, selected?: boolean}) {
+    return <Link href={path}>
+        <div className={[styles.navLink, selected ? styles.selectedLink : undefined].join(" ")}>
+            <Image src={icon} height={24} width={24} alt={caption} />
+            <span className={styles.linkCaption}>{caption}</span>
+        </div>
+    </Link>;
+}
